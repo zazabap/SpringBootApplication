e8ffecfdd811b9142b6d7664001c9aa77c00baf6
Shiwen An <sweynan@icloud.com>
2022-08-02 19:28:39 +0900

    Spring Security with username and password registered on Spring Boot Admin.

---
+++ src/main/java/com/example/sbawebtest/SbaWebTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.sbawebtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SbaWebTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbaWebTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbawebtest/SbaWebTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbawebtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbaWebTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
